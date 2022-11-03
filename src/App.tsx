6e1d93c586b85c66fd1eb69c647f468d56ee6459
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-03 12:27:04 +0900

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import styled from "styled-components";
-import { Route,Routes } from "react-router-dom";
+import styled from 'styled-components'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <Routes>
-  </Routes>
+  return <Routes></Routes>
 }
 
-export default App;
+export default App
